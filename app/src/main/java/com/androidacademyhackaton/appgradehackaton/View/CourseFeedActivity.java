40606c408d43415ab085cc5b94bc4a13729ecb7e
udiValer <udi.valer@gmail.com>
2018-03-15 22:29:08 +0200

    Add courseFeed

---
+++ app/src/main/java/com/androidacademyhackaton/appgradehackaton/View/CourseFeedActivity.java
@@ -0,0 +1,16 @@
+package com.androidacademyhackaton.appgradehackaton.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Udi on 3/15/2018.
+ */
+
+public class CourseFeedActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
